d8f69ade22d31ed2f565d3f524e68ce0951354e9
Dominik277 <dominikdoric277@gmail.com>
2020-12-19 13:04:32 +0100

    Implementirane klase ContextModule i MainActivityContextModule.

--- app/src/main/java/DI/module/ContextModule.java
+++ app/src/main/java/DI/module/ContextModule.java
@@ -1,4 +1,24 @@
 package DI.module;
 
+import android.content.Context;
+
+import DI.qualifier.ApplicationContext;
+import DI.scopes.ApplicationScope;
+import dagger.Module;
+import dagger.Provides;
+
+@Module
 public class ContextModule {
+    private Context context;
+
+    public ContextModule(Context context){
+        this.context = context;
+    }
+
+    @Provides
+    @ApplicationScope
+    @ApplicationContext
+    public Context provideContext(){
+        return context;
+    }
 }
--- app/src/main/java/DI/module/MainActivityContextModule.java
+++ app/src/main/java/DI/module/MainActivityContextModule.java
@@ -1,4 +1,34 @@
 package DI.module;
 
+import android.content.Context;
+
+import DI.qualifier.ActivityContext;
+import DI.scopes.ActivityScope;
+import dagger.Module;
+import dagger.Provides;
+import ui.MainActivity;
+
+@Module
 public class MainActivityContextModule {
+    private MainActivity mainActivity;
+
+    public Context context;
+
+    public MainActivityContextModule(MainActivity mainActivity){
+        this.mainActivity = mainActivity;
+        context = mainActivity;
+    }
+
+    @Provides
+    @ActivityScope
+    public MainActivity providesMainActivity(){
+        return mainActivity;
+    }
+
+    @Provides
+    @ActivityScope
+    @ActivityContext
+    public Context provideContext(){
+        return context;
+    }
 }
