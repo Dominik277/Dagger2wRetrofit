758dfa29c84f476de2601c6a68670842b952b38f
Dominik277 <dominikdoric277@gmail.com>
2020-12-19 13:27:09 +0100

    Implementirana klasa AdapterModule.

--- app/src/main/java/DI/module/AdapterModule.java
+++ app/src/main/java/DI/module/AdapterModule.java
@@ -1,4 +1,24 @@
 package DI.module;
 
+import DI.scopes.ActivityScope;
+import adapter.RecyclerViewAdapter;
+import dagger.Module;
+import dagger.Provides;
+import ui.MainActivity;
+
+@Module(includes = {MainActivityContextModule.class})
 public class AdapterModule {
+
+    @Provides
+    @ActivityScope
+    public RecyclerViewAdapter getStarWarsPeopleList(RecyclerViewAdapter.ClickListener clickListener){
+        return new RecyclerViewAdapter(clickListener);
+    }
+
+    @Provides
+    @ActivityScope
+    public RecyclerViewAdapter.ClickListener getClickLitener(MainActivity mainActivity){
+        return mainActivity;
+    }
+
 }
