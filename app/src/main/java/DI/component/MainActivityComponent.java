527dce01ba7258f20c2a833c7550af164e5ea999
Dominik277 <dominikdoric277@gmail.com>
2020-12-20 14:28:21 +0100

    Implementiran intefejs MainActivityComponent.

--- app/src/main/java/DI/component/MainActivityComponent.java
+++ app/src/main/java/DI/component/MainActivityComponent.java
@@ -1,4 +1,20 @@
 package DI.component;
 
+import android.content.Context;
+
+import DI.module.AdapterModule;
+import DI.qualifier.ActivityContext;
+import DI.scopes.ApplicationScope;
+import dagger.Component;
+import ui.MainActivity;
+
+@ApplicationScope
+@Component(modules = AdapterModule.class,dependencies = ApplicationComponent.class)
 public interface MainActivityComponent {
+
+    @ActivityContext
+    Context getContext();
+
+    void injectMainActivity(MainActivity mainActivity);
+
 }
