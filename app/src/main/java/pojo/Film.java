0ded10d3461d478d31b8f74ce4ba8a4a217090d1
Dominik277 <dominikdoric277@gmail.com>
2020-12-20 14:12:22 +0100

    Implementirana POJO klasa Film.

--- app/src/main/java/pojo/Film.java
+++ app/src/main/java/pojo/Film.java
@@ -1,4 +1,13 @@
 package pojo;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Film {
+
+    @SerializedName("title")
+    public String title;
+
+    @SerializedName("director")
+    public String director;
+
 }
