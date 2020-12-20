9f5df9707c708be926129bc976b267d180b552e6
Dominik277 <dominikdoric277@gmail.com>
2020-12-20 14:29:54 +0100

    Implementiran intefejs DetailActivityComponent.

--- app/src/main/java/DI/component/DetailActivityComponent.java
+++ app/src/main/java/DI/component/DetailActivityComponent.java
@@ -1,4 +1,13 @@
 package DI.component;
 
+import DI.scopes.ActivityScope;
+import dagger.Component;
+import ui.DetailActivity;
+
+@Component(dependencies = ApplicationComponent.class)
+@ActivityScope
 public interface DetailActivityComponent {
+
+    void inject(DetailActivity detailActivity);
+
 }
