75e820b5c39b20c054e35e3972d44d4acdfc814d
Dominik277 <dominikdoric277@gmail.com>
2020-12-20 14:15:31 +0100

    Implementiran intefejs ApplicationComponent.

--- app/src/main/java/DI/component/ApplicationComponent.java
+++ app/src/main/java/DI/component/ApplicationComponent.java
@@ -1,4 +1,24 @@
 package DI.component;
 
+import android.content.Context;
+
+import DI.module.ContextModule;
+import DI.module.RetrofitModule;
+import DI.qualifier.ApplicationContext;
+import DI.scopes.ApplicationScope;
+import dagger.Component;
+import retrofit.APIIinterface;
+import ui.MyApplication;
+
+@ApplicationScope
+@Component(modules = {ContextModule.class, RetrofitModule.class})
 public interface ApplicationComponent {
+
+    public APIIinterface getApiInterface();
+
+    @ApplicationContext
+    public Context getContext();
+
+    public void injectApplication(MyApplication myApplication);
+
 }
