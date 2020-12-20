bd5bafeeb9181022abc58c98d46868f9fb1498dc
Dominik277 <dominikdoric277@gmail.com>
2020-12-20 15:19:22 +0100

    Zavrsena klasa RecyclerViewAdapter.

--- app/src/main/java/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/adapter/RecyclerViewAdapter.java
@@ -1,31 +1,81 @@
 package adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
+import pojo.StarWars;
+import ui.R;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private List<StarWars.People> data;
+    private RecyclerViewAdapter.ClickListener clickListener;
+
+    @Inject
+    public RecyclerViewAdapter(ClickListener clickListener){
+        this.clickListener = clickListener;
+        data = new ArrayList<>();
+    }
+
     @NonNull
     @Override
     public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        return new ViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recycler_view_list_row,parent,false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder holder, int position) {
-
+        holder.txtName.setText(data.get(position).name);
+        holder.txtBirthYear.setText(data.get(position).birthYear);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.size();
     }
 
     class ViewHolder extends RecyclerView.ViewHolder{
 
+        private TextView txtName;
+        private TextView txtBirthYear;
+        private ConstraintLayout constraintLayoutContainer;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            txtName = itemView.findViewById(R.id.txtName);
+            txtBirthYear = itemView.findViewById(R.id.txtBirthYear);
+            constraintLayoutContainer = itemView.findViewById(R.id.constraintLayout);
+
+            constraintLayoutContainer.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    clickListener.launchIntent(data.get(getAdapterPosition()).films.get(0));
+
+                }
+            });
         }
     }
+
+    public interface ClickListener{
+        void launchIntent(String filmName);
+    }
+
+    public void setData(List<StarWars.People> data){
+        this.data.addAll(data);
+        notifyDataSetChanged();
+    }
+
 }
