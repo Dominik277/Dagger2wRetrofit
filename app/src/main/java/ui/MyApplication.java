5d775131f0ca58c5b22082e1ef591b51710866ee
Dominik277 <dominikdoric277@gmail.com>
2020-12-20 14:36:23 +0100

    Implementirana klasa MyApplication.

--- app/src/main/java/ui/MyApplication.java
+++ app/src/main/java/ui/MyApplication.java
@@ -1,4 +1,29 @@
 package ui;
 
-public class MyApplication {
+import android.app.Activity;
+import android.app.Application;
+
+import DI.component.ApplicationComponent;
+import DI.module.ContextModule;
+
+public class MyApplication extends Application {
+
+    ApplicationComponent applicationComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        applicationComponent = DaggerApplicationComponent.builder()
+                .contextModule(new ContextModule(this).build);
+        applicationComponent.injectApplication(this);
+    }
+
+    public static MyApplication get(Activity activity){
+        return (MyApplication) activity.getApplication();
+    }
+
+    public ApplicationComponent getApplicationComponent(){
+        return applicationComponent;
+    }
 }
